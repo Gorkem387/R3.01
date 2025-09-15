dace8f788c0425a327766065e9aa04738a30e9cb
Gorkem387 <gorkemyildiz7ds@gmail.com>
2025-09-15 17:46:51 +0200

    maj shifimu

--- shifumi.js
+++ shifumi.js
@@ -1,2 +1,15 @@
 const choix = [pierre, feuille, ciseaux];
 
+document.getElementById('pierre').addEventListener('click', function() {
+    joueurChoisit('pierre');
+});
+document.getElementById('papier').addEventListener('click', function() {
+    joueurChoisit('feuille');
+});
+document.getElementById('ciseaux').addEventListener('click', function() {
+    joueurChoisit('ciseaux');
+});
+
+function joueurChoisit(choix) {
+    document.getElementById('choix').textContent = choix;
+}
