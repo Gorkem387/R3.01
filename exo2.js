92f20f06c505659fd78168fb9a4885533fb1270c
Gorkem387 <gorkemyildiz7ds@gmail.com>
2025-09-15 16:58:08 +0200

    modif ex2 et html

--- exo2.js
+++ exo2.js
@@ -2,6 +2,6 @@ let longueur = 5;
 let largeur = 3;
 let aire = longueur * largeur;
 
-document.getElementById('longueur').textContent = longueur;
-document.getElementById('largeur').textContent = largeur;
-document.getElementById('aire').textContent = aire;
+console.log(longueur);
+console.log(largeur);
+console.log(aire);
