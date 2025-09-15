f393966754b6cc3ed264fc49738918edbf9592fa
Gorkem387 <gorkemyildiz7ds@gmail.com>
2025-09-15 16:52:20 +0200

    affichage de l'aire du rectangle

---
+++ exo2.js
@@ -0,0 +1,7 @@
+let longueur = 5;
+let largeur = 3;
+let aire = longueur * largeur;
+
+document.getElementById('longueur').textContent = longueur;
+document.getElementById('largeur').textContent = largeur;
+document.getElementById('aire').textContent = aire;
